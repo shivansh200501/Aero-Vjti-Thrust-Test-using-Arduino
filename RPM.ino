d829c15d745c72b0f536e90c0cfcbe94a102a915
Shivansh Shetty <118676909+shivansh200501@users.noreply.github.com>
2023-08-14 21:17:30 +0530

    Update RPM.ino

--- RPM.ino
+++ RPM.ino
@@ -1,41 +1,36 @@
-  
-float value=0;
-float rev=0;
-int rpm=0;
-int oldtime=0;
-int time;
- 
-void isr() //interrupt service routine
+// RPM Measurement using Interrupts
+
+// Define variables
+float value = 0;
+float rev = 0;
+int rpm = 0;
+int oldTime = 0;
+int currentTime;
+
+// Interrupt service routine
+void isr()
 {
-rev++;
+  rev++; // Increment revolution count
 }
- 
+
 void setup()
 {
-Serial.begin(9600);; 
-attachInterrupt(0,isr,RISING); //attaching the interru(pin,OUTPUT);
-
+  Serial.begin(9600); // Initialize serial communication
+  attachInterrupt(0, isr, RISING); // Attach interrupt to rising edge of signal
 }
- 
+
 void loop()
 {
-  
-delay(1000);
-detachInterrupt(0); //detaches the interrupt
-time =millis()-oldtime; //finds the time
-rpm=(rev/time)*60000/2; //calculates rpm for blades  // 1 rev==> x 
-                                                     // y rev==> 60000
-oldtime=millis(); //saves the current time
-rev=0;
-//Serial.clear;
+  delay(1000); // Wait for 1 second
+  detachInterrupt(0); // Detach the interrupt to prevent interference
+  currentTime = millis() - oldTime; // Calculate elapsed time since last loop
+  rpm = (rev / currentTime) * 60000 / 2; // Calculate RPM for blades (divide by 2 for one interrupt per revolution)
+  oldTime = millis(); // Update the old time for the next loop
+  rev = 0; // Reset revolution count
 
-Serial.print( rpm);
-Serial.println(" RPM");
-attachInterrupt(0,isr,RISING);
+  // Print RPM value to serial monitor
+  Serial.print(rpm);
+  Serial.println(" RPM");
 
-
-
-
- 
-  
+  attachInterrupt(0, isr, RISING); // Reattach the interrupt for the next loop
 }
